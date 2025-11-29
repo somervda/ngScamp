f4b597f6f7a2986b8130a17f6030b62e99520240
David <39265971+somervda@users.noreply.github.com>
2025-11-28 19:37:54 -0500

    Working better

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,21 +1,34 @@
 import { Component } from '@angular/core';
 import { WebagentService } from '../services/webagent.service';
+import { FormGroup, FormControl } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-chat',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './chat.component.html',
-  styleUrl: './chat.component.scss'
+  styleUrl: './chat.component.scss',
 })
-
 export class ChatComponent {
+  agentResponse = '';
+  myForm: FormGroup;
 
-    constructor(
-    private webagent: WebagentService){
-      this.webagent.getChat("Hi").subscribe(result =>{console.log(result)});
-    }
-
-
-
+  constructor(private webagent: WebagentService) {
+    this.myForm = new FormGroup({
+      prompt: new FormControl(''), // Initial value can be set here
+    });
   }
+
+  agentChat(prompt: string) {
+    this.webagent.getChat(prompt).subscribe((result) => {
+      console.log(result);
+      this.agentResponse = result;
+    });
+  }
+  sendPrompt() {
+    this.agentResponse = '';
+    this.agentChat(this.myForm.value.prompt);
+    // Perform desired actions here
+  }
+}
