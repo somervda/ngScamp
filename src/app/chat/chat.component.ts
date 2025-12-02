e12318a0acc74b3b545ca607649052aec9b72d1f
David SurfacePro <somervda@comcast.net>
2025-12-02 09:16:44 -0500

    Added a wait cursor

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -3,6 +3,8 @@ import { WebagentService } from '../services/webagent.service';
 import { FormGroup, FormControl } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 import { marked } from 'marked';
+import { DOCUMENT } from '@angular/common';
+import { Inject, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-chat',
@@ -14,17 +16,25 @@ import { marked } from 'marked';
 export class ChatComponent {
   agentResponse = '';
   myForm: FormGroup;
+  wait=false;
 
-  constructor(private webagent: WebagentService) {
+  constructor(private webagent: WebagentService,     
+    @Inject(DOCUMENT) private document: Document,
+    private renderer: Renderer2) {
     this.myForm = new FormGroup({
       prompt: new FormControl(''), // Initial value can be set here
     });
   }
 
   agentChat(prompt: string) {
+    // this.cursorService.showWaitingCursor()
+    this.renderer.addClass(this.document.body, 'waiting-cursor');
+    this.wait=true;
     this.webagent.getChat(prompt).subscribe((data) => {
       console.log(data);
       this.agentResponse = marked(data, { async: false });
+      this.renderer.removeClass(this.document.body, 'waiting-cursor');
+      this.wait=false;
     });
   }
   sendPrompt() {
